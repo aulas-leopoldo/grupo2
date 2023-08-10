779cd27b0d7d0f75e15baa3a9369f3c503afc312
Rafael <akafael@users.noreply.github.com>
2023-08-10 16:42:24 -0300

    Remove function multiplication

--- scm.c
+++ scm.c
@@ -4,13 +4,8 @@ void soma(int a, int b) {
     printf("%d + %d = %d\n", a, b, a + b);
 }
 
-void multiplica(int a, int b) {
-    printf("%d x %d = %d\n", a, b, a * b);
-}
-
 int main() {
     printf("Olá, Estudante!\n");
     soma(5, 3);
-    multiplica(5, 3);
     return 0;
 }
